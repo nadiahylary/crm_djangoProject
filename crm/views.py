b218504ae6200c13b1333207de99d639b0e24861
Nadia HYLARY <nhylary12@gmail.com>
2023-04-19 22:13:37 +0100

    login and logout views done

--- crm/views.py
+++ crm/views.py
@@ -1,20 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
+# allow access to index page if user is logged in
 
+
+@login_required(login_url='login')
 def index(request):
-    return render(request, 'crm/index.html')
+    return render(request, 'crm/index.html', {
+
+    })
 
 
 def login_user(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Successfully Logged in!")
+            return redirect('/')
+        else:
+            messages.error(request, "There was an error. Please try again...or signup!")
+            return redirect('/')
+    else:
+        return render(request, 'crm/login.html')
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You Successfully Logged Out! See you next time ;)")
+    return redirect('/login')
 
 
 def register_user(request):
     pass
-
