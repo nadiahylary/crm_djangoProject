f0be88511baab608204207fbd4a458cca9810849
Nadia HYLARY <nhylary12@gmail.com>
2023-04-20 09:58:20 +0100

    added models

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=15)
+    zipcode = models.CharField(max_length=10)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
