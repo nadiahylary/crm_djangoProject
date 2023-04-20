d5585ee5e9c150fefd54798dd5a5896f5adc694f
Nadia HYLARY <nhylary12@gmail.com>
2023-04-20 01:26:23 +0100

    registration view done!

--- crm/views.py
+++ crm/views.py
@@ -5,6 +5,8 @@ from django.core.validators import validate_email
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
+from crm.forms import SignUpForm
+
 
 # Create your views here.
 
@@ -18,16 +20,16 @@ def index(request):
 
 def login_user(request):
     if request.method == 'POST':
-        username = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "You Successfully Logged in!")
-            return redirect('/')
+            return redirect('index')
         else:
             messages.error(request, "There was an error. Please try again...or signup!")
-            return redirect('/')
+            return redirect('index')
     else:
         return render(request, 'crm/login.html')
 
@@ -40,22 +42,17 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == 'POST':
-        username = request.POST['email']
-        password = request.POST['password']
-        conf_password = request.POST['conf_password']
-        try:
-            validate_email(username)
-            valid_email = True
-        except validate_email.ValidationError:
-            valid_email = False
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Auto authenticate the user:
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, "You Successfully Logged in!")
-            return redirect('/')
-        else:
-            messages.error(request, "There was an error. Please try again...or signup!")
-            return redirect('/')
+            messages.success(request, "Your account was Successfully Created! Cheers!")
+            return redirect('index')
     else:
-        return render(request, 'crm/signup.html')
+        form = SignUpForm()
+        return render(request, 'crm/signup.html', {'form': form})
+    return render(request, 'crm/signup.html', {'form': form})
